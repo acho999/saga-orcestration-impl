92aa2d83decf9da317426167dcc2a82870f4fd34
v-angel.zahariev <v-angel.zahariev@shutterfly.com>
2021-10-11 17:29:16 +0300

    adding builder pattern

--- order-service/src/main/java/com/angel/orderservice/configuration/OrdersServiceConfig.java
+++ order-service/src/main/java/com/angel/orderservice/configuration/OrdersServiceConfig.java
@@ -1,4 +1,4 @@
-package com.angel.orderservice.Configuration;
+package com.angel.orderservice.configuration;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
