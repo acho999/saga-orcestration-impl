361cfed315436f92129e41ee5bc1736a648bdab1
v-angel.zahariev <v-angel.zahariev@shutterfly.com>
2021-10-11 14:05:02 +0300

    adding dependencies and services

--- order-service/src/main/java/com/angel/orderservice/services/OrdersServiceImpl.java
+++ order-service/src/main/java/com/angel/orderservice/services/OrdersServiceImpl.java
@@ -2,8 +2,10 @@ package com.angel.orderservice.services;
 
 import com.angel.orderservice.models.Order;
 import com.angel.orderservice.repos.OrdersRepo;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 
@@ -13,8 +15,13 @@ public class OrdersServiceImpl implements OrdersService {
     @Autowired
     private OrdersRepo repo;
 
+    @Autowired
+    private ModelMapper mapper;
+
     @Override
+    @Transactional
     public Collection<Order> getAll() {
+
         return null;
     }
 }
