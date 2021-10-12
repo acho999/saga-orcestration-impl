adefba5282aeb0bd930a4df25afe520e9a05c929
v-angel.zahariev <v-angel.zahariev@shutterfly.com>
2021-10-12 19:29:36 +0300

    adding orchestration DTO

--- models/src/main/java/DTO/OrchestrationRequestDTO.java
+++ models/src/main/java/DTO/OrchestrationRequestDTO.java
@@ -2,4 +2,50 @@ package DTO;
 
 public class OrchestrationRequestDTO {
 
+    private Integer userId;
+    private Integer productId;
+    private String orderId;
+    private Double amount;
+
+    public static class Builder {
+
+        private Integer userId;
+        private String orderId;
+        private Double amount;
+        private Integer productId;
+
+        public Builder setOrderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder setAmount(Double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder setUserId(Integer userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setProductId(Integer productId) {
+            this.productId = productId;
+            return this;
+        }
+
+        public OrchestrationRequestDTO build() {
+            return new OrchestrationRequestDTO(this);
+        }
+
+
+    }
+
+    private OrchestrationRequestDTO(Builder builder){
+        this.orderId = builder.orderId;
+        this.userId = builder.userId;
+        this.amount = builder.amount;
+        this.productId = builder.productId;
+    }
+
 }
--- models/src/main/java/DTO/OrchestrationResponseDTO.java
+++ models/src/main/java/DTO/OrchestrationResponseDTO.java
@@ -1,5 +1,61 @@
 package DTO;
 
+import states.OrderState;
+
 public class OrchestrationResponseDTO {
+
+    private Integer userId;
+    private Integer productId;
+    private String orderId;
+    private Double amount;
+    private OrderState state;
+
+    public static class Builder {
+
+        private Integer userId;
+        private String orderId;
+        private Double amount;
+        private Integer productId;
+        private OrderState state;
+
+        public Builder setOrderId(String orderId) {
+            this.orderId = orderId;
+            return this;
+        }
+
+        public Builder setOrderId(OrderState state) {
+            this.state = state;
+            return this;
+        }
+
+        public Builder setAmount(Double amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        public Builder setUserId(Integer userId) {
+            this.userId = userId;
+            return this;
+        }
+
+        public Builder setProductId(Integer productId) {
+            this.productId = productId;
+            return this;
+        }
+
+        public OrchestrationResponseDTO build() {
+            return new OrchestrationResponseDTO(this);
+        }
+
+
+    }
+
+    private OrchestrationResponseDTO (Builder builder){
+        this.orderId = builder.orderId;
+        this.userId = builder.userId;
+        this.amount = builder.amount;
+        this.productId = builder.productId;
+        this.state = builder.state;
+    }
 
 }
