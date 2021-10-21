92181a1cc471f2fbb7cfef6c52e9ef6f88044ed9
Angel Zahariev <angel1984zahariev@abv.bg>
2021-10-21 12:47:51 +0300

    changing some dependencies and remove webflux (#6)
    Co-authored-by: v-angel.zahariev <v-angel.zahariev@shutterfly.com>

--- order-service/src/main/java/com/angel/orderservice/controllers/OrdersController.java
+++ order-service/src/main/java/com/angel/orderservice/controllers/OrdersController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/orders")
@@ -18,7 +17,7 @@ public class OrdersController {
     private OrdersService service;
 
     @RequestMapping(method = RequestMethod.GET, value = "/{orderId}")
-    public ResponseEntity<Mono<Order>> getOrder(@PathVariable String id) {
+    public ResponseEntity<Order> getOrder(@PathVariable String id) {
         return null;
     }
 
--- order-service/src/main/java/com/angel/orderservice/services/OrdersService.java
+++ order-service/src/main/java/com/angel/orderservice/services/OrdersService.java
@@ -1,13 +1,12 @@
 package com.angel.orderservice.services;
 
 import com.angel.orderservice.models.Order;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 
 import java.util.Collection;
+import java.util.List;
 
 public interface OrdersService {
 
-    Flux<Order> getAll();
-    Mono<Order> getOrder(String id);
+    List<Order> getAll();
+    List<Order> getOrder(String id);
 }
